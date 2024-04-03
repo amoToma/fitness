156b9176395dfeaab9884c343ecfa907734e1727
Toma Kazanskaya <tkazanskaya888@gmail.com>
2024-04-03 17:50:23 +0300

    добавляет скрипт для табов

--- source/js/main.js
+++ source/js/main.js
@@ -6,4 +6,31 @@ import { setupVideo } from './video';
 
 setupVideo();
 
+const price = document.querySelector('.price');
+const tabButtons = Array.from(price.querySelectorAll('.tabs__navigation-button'));
+const cardLists = Array.from(price.querySelectorAll('.tabs__cards-list'));
 
+let indexList = 0;
+
+const showCardList = (index) => {
+  cardLists.forEach((item) => {
+    item.style.display = 'none';
+  });
+  cardLists[index].style.display = 'grid';
+};
+
+const showActiveTabs = (index) => {
+  tabButtons.forEach((item) => item.classList.remove('tabs__navigation-button--current'));
+  tabButtons[index].classList.add('tabs__navigation-button--current');
+};
+
+tabButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    indexList = index;
+    showActiveTabs(indexList);
+    showCardList(indexList);
+  });
+});
+
+showActiveTabs(indexList);
+showCardList(indexList);
