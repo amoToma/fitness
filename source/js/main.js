bd46819054459317e3f90ca4c7462589d1c6a294
Toma Kazanskaya <tkazanskaya888@gmail.com>
2024-04-06 22:09:12 +0300

    добавляет слайдер

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 // https://swiperjs.com/get-started#installation
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+
 import { setupVideo } from './modules/video';
 import { showActiveTabs, showCardList, indexList } from './modules/price';
 import { showCurrentButton, showQuestionsBlock, blockIndex } from './modules/faq';
@@ -14,3 +14,34 @@ showCurrentButton(blockIndex);
 showQuestionsBlock(blockIndex);
 openAccordion();
 
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+
+const juriSwiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  modules: [ Navigation ],
+
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    }
+  }
+});
